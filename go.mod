7e9b96cf5e0b1d523780840bc4e4d986c2bb4ee6
Botao <wxt2005@gmail.com>
2023-07-23 20:39:07 +0900

    Fix twitter

--- go.mod
+++ go.mod
@@ -5,14 +5,9 @@ go 1.13
 replace github.com/search2d/go-pixiv => ./vendor/go-pixiv
 
 require (
-	github.com/ChimeraCoder/anaconda v2.0.0+incompatible
-	github.com/ChimeraCoder/tokenbucket v0.0.0-20131201223612-c5a927568de7 // indirect
-	github.com/azr/backoff v0.0.0-20160115115103-53511d3c7330 // indirect
 	github.com/dropbox/dropbox-sdk-go-unofficial v5.6.0+incompatible
-	github.com/dustin/go-jsonpointer v0.0.0-20160814072949-ba0abeacc3dc // indirect
-	github.com/dustin/gojson v0.0.0-20160307161227-2e71ec9dd5ad // indirect
-	github.com/garyburd/go-oauth v0.0.0-20180319155456-bca2e7f09a17 // indirect
 	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1
+	github.com/google/uuid v1.3.0
 	github.com/h2non/bimg v1.1.9
 	github.com/pelletier/go-toml/v2 v2.0.9 // indirect
 	github.com/search2d/go-pixiv v0.0.0-00010101000000-000000000000
--- service/twitter.go
+++ service/twitter.go
@@ -1,13 +1,16 @@
 package service
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
-	"net/url"
+	"io/ioutil"
+	"net/http"
 	"regexp"
 	"strconv"
 	"strings"
 
-	"github.com/ChimeraCoder/anaconda"
+	"github.com/google/uuid"
 	"github.com/spf13/viper"
 )
 
@@ -15,27 +18,29 @@ const mp4ContentType = "video/mp4"
 const twitterUserPrefix = "https://twitter.com/"
 
 type TwitterService struct {
-	Service   Type
-	urlRegexp *regexp.Regexp
-	client    *anaconda.TwitterApi
+	Service     Type
+	urlRegexp   *regexp.Regexp
+	bearerToken string
+	authToken   string
+	client      *http.Client
 }
 
 func NewTwitterService() *TwitterService {
-	consumerKey := viper.GetString("twitter.consumer_key")
-	consumerSecret := viper.GetString("twitter.consumer_secret")
-	accessTokenKey := viper.GetString("twitter.access_token_key")
-	accessTokenSecret := viper.GetString("twitter.access_token_secret")
-	client := anaconda.NewTwitterApiWithCredentials(accessTokenKey, accessTokenSecret, consumerKey, consumerSecret)
+	bearerToken := viper.GetString("twitter.bearer_token")
+	authToken := viper.GetString("twitter.auth_token")
 
 	return &TwitterService{
-		Service:   Twitter,
-		urlRegexp: regexp.MustCompile(`(?i)https?:\/\/(?:(?:www|mobile)\.)?(?:vx|fx)?twitter\.com\/(.+?)\/status\/(\d+)`),
-		client:    client,
+		Service:     Twitter,
+		urlRegexp:   regexp.MustCompile(`(?i)https?:\/\/(?:(?:www|mobile)\.)?(?:vx|fx)?twitter\.com\/(.+?)\/status\/(\d+)`),
+		bearerToken: bearerToken,
+		authToken:   authToken,
+		client:      &http.Client{},
 	}
 }
 
 func (s TwitterService) CheckValid(urlString string) (*IncomingURL, bool) {
 	match := s.urlRegexp.FindStringSubmatch(urlString)
+
 	if match == nil {
 		return nil, false
 	}
@@ -57,23 +62,124 @@ func (s TwitterService) IsService(serviceType Type) bool {
 	return serviceType == s.Service
 }
 
+type TweetResponse struct {
+	Data struct {
+		ThreadedConversationWithInjectionsV2 struct {
+			Instructions []json.RawMessage
+		} `json:"threaded_conversation_with_injections_v2"`
+	}
+}
+
+type TimelineAddEntries struct {
+	Entries []json.RawMessage
+}
+
+type TweetEntity struct {
+	Content struct {
+		ItemContent struct {
+			TweetResults struct {
+				Result struct {
+					Core struct {
+						UserResults struct {
+							Result struct {
+								Legacy struct {
+									Name       string
+									ScreenName string `json:"screen_name"`
+								}
+							}
+						} `json:"user_results"`
+					}
+					Legacy struct {
+						FullText         string `json:"full_text"`
+						DisplayTextRange []int  `json:"display_text_range"`
+						Entities         struct {
+							Media []EntityMedia
+						}
+						ExtendedEntities struct {
+							Media []EntityMedia
+						} `json:"extended_entities"`
+					}
+				}
+			} `json:"tweet_results"`
+		}
+	}
+}
+
+type EntityMedia struct {
+	Type          string
+	MediaUrlHttps string `json:"media_url_https"`
+	VideoInfo     struct {
+		Variants []struct {
+			ContentType string `json:"content_type"`
+			Url         string
+		}
+	} `json:"video_info"`
+}
+
 func (s TwitterService) ExtractMediaFromURL(incomingURL *IncomingURL) ([]*Media, error) {
 	var result []*Media
-	client := s.client
-	id := incomingURL.IntID
 
-	if id == 0 {
-		return result, nil
-	}
-
-	tweet, err := client.GetTweet(int64(id), url.Values{"tweet_mode": []string{"extended"}})
+	req, err := http.NewRequest("GET", "https://twitter.com/i/api/graphql/q94uRCEn65LZThakYcPT6g/TweetDetail", nil)
 	if err != nil {
-		return nil, err
+		return result, err
 	}
 
-	mediaEntities := tweet.Entities.Media
-	if len(tweet.ExtendedEntities.Media) >= len(mediaEntities) {
-		mediaEntities = tweet.ExtendedEntities.Media
+	csrfToken := strings.Replace(uuid.New().String(), "-", "", -1)
+
+	req.Header.Set("Authorization", "Bearer "+s.bearerToken)
+	req.Header.Set("Cookie", fmt.Sprintf("ct0=%s; auth_token=%s", csrfToken, s.authToken))
+	req.Header.Set("x-csrf-token", csrfToken)
+	req.Header.Set("x-twitter-auth-type", "OAuth2Session")
+	req.Header.Set("x-twitter-client-language", "en")
+	req.Header.Set("x-twitter-active-user", "yes")
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/115.0")
+
+	q := req.URL.Query()
+	q.Add("features", (`{"rweb_lists_timeline_redesign_enabled":true,"responsive_web_graphql_exclude_directive_enabled":true,"verified_phone_label_enabled":false,"creator_subscriptions_tweet_preview_api_enabled":true,"responsive_web_graphql_timeline_navigation_enabled":true,"responsive_web_graphql_skip_user_profile_image_extensions_enabled":false,"tweetypie_unmention_optimization_enabled":true,"responsive_web_edit_tweet_api_enabled":true,"graphql_is_translatable_rweb_tweet_is_translatable_enabled":true,"view_counts_everywhere_api_enabled":true,"longform_notetweets_consumption_enabled":true,"responsive_web_twitter_article_tweet_consumption_enabled":false,"tweet_awards_web_tipping_enabled":false,"freedom_of_speech_not_reach_fetch_enabled":true,"standardized_nudges_misinfo":true,"tweet_with_visibility_results_prefer_gql_limited_actions_policy_enabled":true,"longform_notetweets_rich_text_read_enabled":true,"longform_notetweets_inline_media_enabled":true,"responsive_web_media_download_video_enabled":false,"responsive_web_enhance_cards_enabled":false}`))
+	q.Add("fieldToggles", (`{"withAuxiliaryUserLabels":false,"withArticleRichContentState":false}`))
+	q.Add("variables", fmt.Sprintf(`{"focalTweetId":"%s","with_rux_injections":false,"includePromotedContent":true,"withCommunity":true,"withQuickPromoteEligibilityTweetFields":true,"withBirdwatchNotes":true,"withVoice":true,"withV2Timeline":true}`, incomingURL.StrID))
+	req.URL.RawQuery = q.Encode()
+
+	client := s.client
+
+	resp, err := client.Do(req)
+
+	if err != nil {
+		print(err.Error())
+		return result, err
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return result, err
+	}
+
+	var tweetResponse TweetResponse
+	err = json.Unmarshal(body, &tweetResponse)
+	if err != nil {
+		fmt.Println(err.Error())
+		return result, err
+	}
+
+	var timelineAddEntries TimelineAddEntries
+	err = json.Unmarshal(tweetResponse.Data.ThreadedConversationWithInjectionsV2.Instructions[0], &timelineAddEntries)
+	if err != nil {
+		return result, errors.New("can't parse TimelineAddEntries")
+	}
+
+	var tweetEntity TweetEntity
+	err = json.Unmarshal(timelineAddEntries.Entries[0], &tweetEntity)
+	if err != nil {
+		return result, errors.New("can't parse TweetEntity")
+	}
+
+	mediaEntities := tweetEntity.Content.ItemContent.TweetResults.Result.Legacy.Entities.Media
+	extendedMediaEntities := tweetEntity.Content.ItemContent.TweetResults.Result.Legacy.ExtendedEntities.Media
+
+	if len(extendedMediaEntities) >= len(mediaEntities) {
+		mediaEntities = extendedMediaEntities
 	}
 
 	for _, mediaEntity := range mediaEntities {
@@ -92,8 +198,8 @@ func (s TwitterService) ExtractMediaFromURL(incomingURL *IncomingURL) ([]*Media,
 
 		resultMedia.Service = string(s.Service)
 		resultMedia.Source = incomingURL.URL
-		resultMedia.FileName = fmt.Sprintf("@%s_%s", tweet.User.ScreenName, resultMedia.FileName)
-		s.completeMediaMeta(resultMedia, &tweet)
+		resultMedia.FileName = fmt.Sprintf("@%s_%s", tweetEntity.Content.ItemContent.TweetResults.Result.Core.UserResults.Result.Legacy.ScreenName, resultMedia.FileName)
+		s.completeMediaMeta(resultMedia, &tweetEntity)
 
 		result = append(result, resultMedia)
 	}
@@ -101,25 +207,25 @@ func (s TwitterService) ExtractMediaFromURL(incomingURL *IncomingURL) ([]*Media,
 	return result, nil
 }
 
-func (s TwitterService) completeMediaMeta(media *Media, tweet *anaconda.Tweet) {
-	media.Author = tweet.User.Name
-	media.AuthorURL = twitterUserPrefix + tweet.User.ScreenName
-	media.Description = string([]rune(tweet.FullText)[tweet.DisplayTextRange[0]:tweet.DisplayTextRange[1]])
+func (s TwitterService) completeMediaMeta(media *Media, tweet *TweetEntity) {
+	media.Author = tweet.Content.ItemContent.TweetResults.Result.Core.UserResults.Result.Legacy.Name
+	media.AuthorURL = twitterUserPrefix + tweet.Content.ItemContent.TweetResults.Result.Core.UserResults.Result.Legacy.ScreenName
+	media.Description = string([]rune(tweet.Content.ItemContent.TweetResults.Result.Legacy.FullText)[tweet.Content.ItemContent.TweetResults.Result.Legacy.DisplayTextRange[0]:tweet.Content.ItemContent.TweetResults.Result.Legacy.DisplayTextRange[1]])
 }
 
-func (s TwitterService) extractPhoto(media *anaconda.EntityMedia) *Media {
-	urlParts := strings.Split(media.Media_url_https, "/")
+func (s TwitterService) extractPhoto(media *EntityMedia) *Media {
+	urlParts := strings.Split(media.MediaUrlHttps, "/")
 	// wxt2005_1.jpg
 	fileName := urlParts[len(urlParts)-1]
 
 	return &Media{
 		FileName: fileName,
-		URL:      media.Media_url_https,
+		URL:      media.MediaUrlHttps,
 		Type:     "photo",
 	}
 }
 
-func (s TwitterService) extractVideo(media *anaconda.EntityMedia) *Media {
+func (s TwitterService) extractVideo(media *EntityMedia) *Media {
 	variants := media.VideoInfo.Variants
 	videoUrl := ""
 	for _, item := range variants {
