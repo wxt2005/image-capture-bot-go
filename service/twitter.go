3fc8b3df065bb761600d5f88e01fb158aaf0352d
Botao <wxt2005@gmail.com>
2025-05-06 12:09:56 +0900

    fix: twitter

--- service/twitter.go
+++ service/twitter.go
@@ -11,6 +11,7 @@ import (
 	"strings"
 
 	"github.com/google/uuid"
+	log "github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 )
 
@@ -48,7 +49,6 @@ func (s TwitterService) CheckValid(urlString string) (*IncomingURL, bool) {
 	strID := match[2]
 	intID, _ := strconv.Atoi(strID)
 	url := fmt.Sprintf("%s%s/status/%s", twitterUserPrefix, match[1], match[2])
-	print(intID)
 
 	return &IncomingURL{
 		Service:  s.Service,
@@ -129,7 +129,7 @@ type EntityMedia struct {
 func (s TwitterService) ExtractMediaFromURL(incomingURL *IncomingURL) ([]*Media, error) {
 	var result []*Media
 
-	req, err := http.NewRequest("GET", "https://twitter.com/i/api/graphql/q94uRCEn65LZThakYcPT6g/TweetDetail", nil)
+	req, err := http.NewRequest("GET", "https://x.com/i/api/graphql/xd_EMdYvB9hfZsZ6Idri0w/TweetDetail", nil)
 	if err != nil {
 		return result, err
 	}
@@ -142,12 +142,13 @@ func (s TwitterService) ExtractMediaFromURL(incomingURL *IncomingURL) ([]*Media,
 	req.Header.Set("x-twitter-auth-type", "OAuth2Session")
 	req.Header.Set("x-twitter-client-language", "en")
 	req.Header.Set("x-twitter-active-user", "yes")
+	req.Header.Set("x-client-transaction-id", "RiYhUS2woF9cK5mEtB5te+NZ6wnqJwGn7OrvSvqIp1Aug/LKhd15QQBoc1czNq0M4BpQjEWHdj1DcNR1SmTjZW9jLQNhRQ")
 	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/115.0")
 
 	q := req.URL.Query()
-	q.Add("features", (`{"rweb_lists_timeline_redesign_enabled":true,"responsive_web_graphql_exclude_directive_enabled":true,"verified_phone_label_enabled":false,"creator_subscriptions_tweet_preview_api_enabled":true,"responsive_web_graphql_timeline_navigation_enabled":true,"responsive_web_graphql_skip_user_profile_image_extensions_enabled":false,"tweetypie_unmention_optimization_enabled":true,"responsive_web_edit_tweet_api_enabled":true,"graphql_is_translatable_rweb_tweet_is_translatable_enabled":true,"view_counts_everywhere_api_enabled":true,"longform_notetweets_consumption_enabled":true,"responsive_web_twitter_article_tweet_consumption_enabled":false,"tweet_awards_web_tipping_enabled":false,"freedom_of_speech_not_reach_fetch_enabled":true,"standardized_nudges_misinfo":true,"tweet_with_visibility_results_prefer_gql_limited_actions_policy_enabled":true,"longform_notetweets_rich_text_read_enabled":true,"longform_notetweets_inline_media_enabled":true,"responsive_web_media_download_video_enabled":false,"responsive_web_enhance_cards_enabled":false}`))
-	q.Add("fieldToggles", (`{"withAuxiliaryUserLabels":false,"withArticleRichContentState":false}`))
-	q.Add("variables", fmt.Sprintf(`{"focalTweetId":"%s","with_rux_injections":false,"includePromotedContent":true,"withCommunity":true,"withQuickPromoteEligibilityTweetFields":true,"withBirdwatchNotes":true,"withVoice":true,"withV2Timeline":true}`, incomingURL.StrID))
+	q.Add("features", (`{"rweb_video_screen_enabled":false,"profile_label_improvements_pcf_label_in_post_enabled":true,"rweb_tipjar_consumption_enabled":true,"verified_phone_label_enabled":false,"creator_subscriptions_tweet_preview_api_enabled":true,"responsive_web_graphql_timeline_navigation_enabled":true,"responsive_web_graphql_skip_user_profile_image_extensions_enabled":false,"premium_content_api_read_enabled":false,"communities_web_enable_tweet_community_results_fetch":true,"c9s_tweet_anatomy_moderator_badge_enabled":true,"responsive_web_grok_analyze_button_fetch_trends_enabled":false,"responsive_web_grok_analyze_post_followups_enabled":true,"responsive_web_jetfuel_frame":false,"responsive_web_grok_share_attachment_enabled":true,"articles_preview_enabled":true,"responsive_web_edit_tweet_api_enabled":true,"graphql_is_translatable_rweb_tweet_is_translatable_enabled":true,"view_counts_everywhere_api_enabled":true,"longform_notetweets_consumption_enabled":true,"responsive_web_twitter_article_tweet_consumption_enabled":true,"tweet_awards_web_tipping_enabled":false,"responsive_web_grok_show_grok_translated_post":false,"responsive_web_grok_analysis_button_from_backend":true,"creator_subscriptions_quote_tweet_preview_enabled":false,"freedom_of_speech_not_reach_fetch_enabled":true,"standardized_nudges_misinfo":true,"tweet_with_visibility_results_prefer_gql_limited_actions_policy_enabled":true,"longform_notetweets_rich_text_read_enabled":true,"longform_notetweets_inline_media_enabled":true,"responsive_web_grok_image_annotation_enabled":true,"responsive_web_enhance_cards_enabled":false}`))
+	q.Add("fieldToggles", (`{"withArticleRichContentState":true,"withArticlePlainText":false,"withGrokAnalyze":false,"withDisallowedReplyControls":false}`))
+	q.Add("variables", fmt.Sprintf(`{"focalTweetId":"%s","referrer":"tweet","with_rux_injections":false,"rankingMode":"Relevance","includePromotedContent":true,"withCommunity":true,"withQuickPromoteEligibilityTweetFields":true,"withBirdwatchNotes":true,"withVoice":true}`, incomingURL.StrID))
 	req.URL.RawQuery = q.Encode()
 
 	client := s.client
@@ -155,7 +156,7 @@ func (s TwitterService) ExtractMediaFromURL(incomingURL *IncomingURL) ([]*Media,
 	resp, err := client.Do(req)
 
 	if err != nil {
-		print(err.Error())
+		log.WithError(err).Error("failed to send request")
 		return result, err
 	}
 
@@ -163,13 +164,22 @@ func (s TwitterService) ExtractMediaFromURL(incomingURL *IncomingURL) ([]*Media,
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
+		log.WithError(err).Error("failed to read response body")
 		return result, err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		log.WithFields(log.Fields{
+			"status_code": resp.StatusCode,
+			"body":        string(body),
+		}).Error("failed to get tweet detail")
+		return result, errors.New("failed to get tweet detail")
 	}
 
 	var tweetResponse TweetResponse
 	err = json.Unmarshal(body, &tweetResponse)
 	if err != nil {
-		fmt.Println(err.Error())
+		log.WithError(err).Error("failed to unmarshal response body")
 		return result, err
 	}
 
